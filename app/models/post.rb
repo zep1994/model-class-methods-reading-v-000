c8673ed421608388dde56eccad3d4cbd8a21d074
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-07 15:55:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
 
   #put new code here
-def self.by_author(author_id)
-  where(author: author_id)
-end
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
 
   private
 
